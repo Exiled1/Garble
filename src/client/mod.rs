78bfe0c6ccf49c04ea457dbf7c436a4e0deb1861
Exiled1 <rudyperalta831@gmail.com>
2022-03-15 23:15:33 -0700

    Changed the ChatClient interface to mimic a two-way channel using one way channels

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 use std::io::Read;
 use bytes::Bytes;
-use futures::prelude::*;
+use futures::{prelude::*, channel::{oneshot::channel}};
 use openssl::{
     envelope::Open,
     error::{Error as SslError, ErrorStack},
@@ -9,7 +9,7 @@ use openssl::{
 };
 use sha3::{Digest, Sha3_512};
 use thiserror::Error;
-use tokio::net::{TcpListener, TcpStream};
+use tokio::{net::{TcpListener, TcpStream}, sync::mpsc::{UnboundedReceiver, UnboundedSender, unbounded_channel}};
 use tokio_stream::StreamExt;
 use tokio_util::codec::{Framed, Decoder};
 use crate::message::{self, MessageCodec, Serverbound, Clientbound};
@@ -28,24 +28,36 @@ pub enum ClientError {
     // HashingError(#[from] )
 }
 
+
+
+// We're gonna need to define the interface from Client to Server & from Server to Client, but can't be same struct b/c of ownership.
 pub struct ChatClient {
-    username: String,
-    port: usize,
-    hostname: String,
-    keypair: Rsa<Private>,
-    tcp_stream: MessageStream,
+    // port: usize,
+    // hostname: String,
+    // keypair: Rsa<Private>,
+    // tcp_stream: MessageStream,
+    pub inbound: UnboundedReceiver<String>,
+    pub outbound: UnboundedSender<String>
 }
 
 impl ChatClient {
     // Makes a connection to the port:hostname, and sets our username, does not set the pub/priv key pair yet.
     pub async fn new(port: usize, hostname: String, username: String) -> Result<Self, ClientError> {
-        
+        // Make a pair of channels for msg from Terminal to Client thread (outbound messages; user sent).
+        // Then from client to terminal (inbound messages; terminal sent).
+        // Goal is to separate inbound from outbound stuff to a background task.
+        let (inbound_tx, inbound_rx) = unbounded_channel();
+        let (outbound_tx, outbound_rx) = unbounded_channel();
+        // tokio::spawn({
+
+        // })
         Ok(ChatClient {
-            username,
-            port,
-            hostname: hostname.clone(),
-            keypair: ChatClient::generate_keypair(None)?,
-            tcp_stream: ChatClient::connect(hostname, port).await?
+            // port,
+            // hostname: hostname.clone(),
+            // keypair: ChatClient::generate_keypair(None)?,
+            // tcp_stream: ChatClient::connect(hostname, port).await?
+            inbound: inbound_rx,
+            outbound: outbound_tx
         })
     }
 
@@ -57,13 +69,13 @@ impl ChatClient {
     }
 
     // Simply hashes the public key and returns the string result of it. This is for identifying a user while giving no information to the server.
-    fn hash_pub_key(&mut self) -> Result<Vec<u8>, ClientError> {
-        let mut hasher = Sha3_512::new();
-        hasher.update(self.keypair.public_key_to_pem().as_ref().unwrap());
-        let hash = hasher.finalize();
-        let hashed_pub_key = hash.bytes().collect::<Result<Vec<u8>, std::io::Error>>()?;
-        Ok(hashed_pub_key)
-    }
+    // fn hash_pub_key(&mut self) -> Result<Vec<u8>, ClientError> {
+    //     let mut hasher = Sha3_512::new();
+    //     hasher.update(self.keypair.public_key_to_pem().as_ref().unwrap());
+    //     let hash = hasher.finalize();
+    //     let hashed_pub_key = hash.bytes().collect::<Result<Vec<u8>, std::io::Error>>()?;
+    //     Ok(hashed_pub_key)
+    // }
 
     // Client hashes the pkey and sends the result to the server, returns Result<(), ClientError>.
     fn server_pkey_exchange(&self) -> Result<(), ClientError> {
